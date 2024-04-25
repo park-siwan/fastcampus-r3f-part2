db1849fa85156978142cb119b9f0afe3117f9979
park-siwan <siwandevelop@gmail.com>
2024-04-25 18:36:46 +0900

    날씨 Open API

--- example02/src/components/Scene.js
+++ example02/src/components/Scene.js
@@ -1,15 +1,22 @@
-import Earth from "./Earth";
-import Weather from "./Weather";
+import { useEffect } from 'react';
+import { getCurrentWeather } from '../utils/weatherApi';
+import Earth from './Earth';
+import Weather from './Weather';
 
 const Scene = () => {
-    return(
-        <>
-            <Earth position={[0,-2,0]}/>
-            <Weather position={[0.5,0,0]} weather={'rain'}/>
-            <Weather position={[0,0,0]} weather={'clear'}/>
-            <Weather position={[-0.5,0,0]} weather={'snow'} />
-        </>
-    )
-}
+  console.log(process.env.REACT_APP_API_KEY);
+  useEffect(() => {
+    getCurrentWeather(44.34, 10.99, process.env.REACT_APP_API_KEY);
+  }, []);
+
+  return (
+    <>
+      <Earth position={[0, -2, 0]} />
+      <Weather position={[0.5, 0, 0]} weather={'rain'} />
+      <Weather position={[0, 0, 0]} weather={'clear'} />
+      <Weather position={[-0.5, 0, 0]} weather={'snow'} />
+    </>
+  );
+};
 
 export default Scene;
