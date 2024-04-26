762f0df0e9cf0f2f2f570d3832cc043217d5b5b3
park-siwan <siwandevelop@gmail.com>
2024-04-26 16:12:39 +0900

    feat: suspense

--- example02/src/pages/index.js
+++ example02/src/pages/index.js
@@ -1,13 +1,33 @@
 import { Canvas } from '@react-three/fiber';
-import Scene from '../components/Scene';
+// import Scene from '../components/Scene';
 import Lights from '../components/Lights';
+import { Suspense, lazy } from 'react';
+
+function Sphere() {
+  return (
+    <mesh>
+      <sphereGeometry args={[1]} />
+      <meshBasicMaterial color='white' />
+    </mesh>
+  );
+}
+
+const Scene = lazy(() => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(import('../components/Scene'));
+    }, 5000);
+  });
+});
 
 function Home() {
   return (
     <Canvas camera={{ position: [0, 0, 5], fov: 45 }}>
       <color attach='background' args={['rgb(67, 127, 240) 100%)']} />
-      <Lights />
-      <Scene />
+      <Suspense fallback={<Sphere />}>
+        <Lights />
+        <Scene />
+      </Suspense>
     </Canvas>
   );
 }
