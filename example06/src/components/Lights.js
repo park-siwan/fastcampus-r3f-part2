5b8bc9fd55ed151943b6fa0a691c155804a9aa93
park-siwan <siwandevelop@gmail.com>
2024-04-30 17:32:31 +0900

    배경색 변화주기, 조명변화

--- example06/src/components/Lights.js
+++ example06/src/components/Lights.js
@@ -1,17 +1,23 @@
-import { Environment, useHelper } from "@react-three/drei";
-import { useRef } from "react";
-import { DirectionalLightHelper } from "three";
+import { Environment, useHelper } from '@react-three/drei';
+import { useRef } from 'react';
+import { DirectionalLightHelper } from 'three';
 
 const Lights = () => {
-    const drectRef = useRef()
-    // useHelper(drectRef, DirectionalLightHelper, 'cyan')
-    return(
-        <>
-            <Environment preset="forest"/>
-            <directionalLight ref={drectRef} position={[0,5,5]} intensity={1.4} castShadow />
-            <ambientLight intensity={1} color="white" />
-        </>
-    )
-}
+  const drectRef = useRef();
+  // useHelper(drectRef, DirectionalLightHelper, 'cyan')
+  return (
+    <>
+      {/* <Environment preset="forest"/> */}
+      <directionalLight
+        ref={drectRef}
+        position={[-5, 5, 5]}
+        intensity={10}
+        castShadow
+        receiveShadow
+      />
+      <ambientLight intensity={1} color='white' />
+    </>
+  );
+};
 
 export default Lights;
